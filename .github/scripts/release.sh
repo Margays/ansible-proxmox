808614dbe98a04da444610f1e8a599fc2560ea45
Łukasz Wencel <65486779+lwencel-priv@users.noreply.github.com>
2025-01-26 22:20:46 +0100

    Create GitHub Action for Ansible Galaxy release (#9)
    * Create GitHub Action for Ansible Galaxy release using Makefile
    
    Add GitHub Action workflow for Ansible Galaxy release using Makefile.
    
    * **Makefile**
      - Add `release` target to call `release.sh` script and handle version bump in `galaxy.yml`.
      - Add `test` target to run unit tests using `tox`.
    
    * **.github/scripts/release.sh**
      - Modify script to use `GALAXY_API_KEY` environment variable.
    
    * **.github/workflows/release.yml**
      - Create new GitHub Action workflow to automate release process.
      - Trigger workflow on push to `main` and on manual dispatch.
      - Include steps to check out repository, set up Python, install dependencies, run tests, and execute `release` target in `Makefile`.
    
    * Refactor release process
    
    * Remove empty line

--- .github/scripts/release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -rf build
-collection="${2%*/}"
-echo "Releasing collection $collection"
-ansible-galaxy collection build --output-path build
-ansible-galaxy collection publish build/margays-$collection-*.tar.gz \
-    --api-key $1 \
-    --server="https://galaxy.ansible.com/"
