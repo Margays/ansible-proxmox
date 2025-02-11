76ea12b43abe292107b8eb505df8c2c5948e7595
Łukasz Wencel <65486779+lwencel-priv@users.noreply.github.com>
2025-02-11 22:22:52 +0100

    Write tests for cluster_acme_plugin_handler.py & cluster_acme_account_handler.py

---
+++ plugins/tests/module_utils/proxmox/handlers/test_cluster_acme_plugin_handler.py
@@ -0,0 +1,127 @@
+import pytest
+from typing import Iterable
+from module_utils.proxmox.handlers.cluster_acme_plugin_handler import ClusterAcmePluginHandler
+from ..utils import create_client, Response
+
+
+@pytest.mark.parametrize(
+    "input_data,responses,changed",
+    [
+        pytest.param(
+            {
+                "id": "test",
+                "type": "dns",
+                "api": "https://api.example.com",
+                "data": {"key": "value"},
+                "disable": False,
+                "nodes": ["node1", "node2"],
+                "validation_delay": 30,
+            },
+            [
+                Response(
+                    command=["/usr/bin/pvesh", "get", "cluster/acme/plugins/test", "--output-format=json"],
+                    return_code=0,
+                    stdout=b'{"id": "test", "type": "dns", "api": "https://api.example.com", "data": {"key": "value"}, "disable": False, "nodes": ["node1", "node2"], "validation-delay": 30}',
+                    stderr=b'',
+                ),
+            ],
+            False,
+            id="not-modification",
+        ),
+        pytest.param(
+            {
+                "id": "test",
+                "type": "dns",
+                "api": "https://api.example.com",
+                "data": {"key": "value"},
+                "disable": False,
+                "nodes": ["node1", "node2"],
+                "validation_delay": 60,
+            },
+            [
+                Response(
+                    command=["/usr/bin/pvesh", "get", "cluster/acme/plugins/test", "--output-format=json"],
+                    return_code=0,
+                    stdout=b'{"id": "test", "type": "dns", "api": "https://api.example.com", "data": {"key": "value"}, "disable": False, "nodes": ["node1", "node2"], "validation-delay": 30}',
+                    stderr=b'',
+                ),
+                Response(
+                    command=["/usr/bin/pvesh", "set", "cluster/acme/plugins/test", "--validation-delay=60", "--output-format=json"],
+                    return_code=0,
+                    stdout=b'',
+                    stderr=b'',
+                ),
+            ],
+            True,
+            id="modified",
+        )
+    ]
+)
+def test_cluster_acme_plugin_handler_modify(input_data: dict, responses: Iterable[Response], changed: bool) -> None:
+    client = create_client(responses)
+    handler = ClusterAcmePluginHandler(client, input_data)
+    ansible_result = handler.modify(check=False)
+    assert ansible_result.status == changed
+    assert client.responses.empty()
+
+
+@pytest.mark.parametrize(
+    "input_data,responses,changed",
+    [
+        pytest.param(
+            {
+                "id": "test",
+                "type": "dns",
+                "api": "https://api.example.com",
+                "data": {"key": "value"},
+                "disable": False,
+                "nodes": ["node1", "node2"],
+                "validation_delay": 30,
+            },
+            [
+                Response(
+                    command=["/usr/bin/pvesh", "create", "cluster/acme/plugins", "--id=test", "--type=dns", "--api=https://api.example.com", "--data=key:value", "--disable=False", "--nodes=node1,node2", "--validation-delay=30", "--output-format=json"],
+                    return_code=0,
+                    stdout=b'{"id": "test", "type": "dns", "api": "https://api.example.com", "data": {"key": "value"}, "disable": False, "nodes": ["node1", "node2"], "validation-delay": 30}',
+                    stderr=b'',
+                ),
+            ],
+            True,
+            id="created",
+        ),
+    ]
+)
+def test_cluster_acme_plugin_handler_create(input_data: dict, responses: Iterable[Response], changed: bool) -> None:
+    client = create_client(responses)
+    handler = ClusterAcmePluginHandler(client, input_data)
+    ansible_result = handler.create(check=False)
+    assert ansible_result.status == changed
+    assert client.responses.empty()
+
+
+@pytest.mark.parametrize(
+    "input_data,responses,changed",
+    [
+        pytest.param(
+            {
+                "id": "test",
+            },
+            [
+                Response(
+                    command=["/usr/bin/pvesh", "delete", "cluster/acme/plugins/test", "--output-format=json"],
+                    return_code=0,
+                    stdout=b'',
+                    stderr=b'',
+                ),
+            ],
+            True,
+            id="existing-resource",
+        ),
+    ]
+)
+def test_cluster_acme_plugin_handler_delete(input_data: dict, responses: Iterable[Response], changed: bool) -> None:
+    client = create_client(responses)
+    handler = ClusterAcmePluginHandler(client, input_data)
+    ansible_result = handler.remove(check=False)
+    assert ansible_result.status == changed
+    assert client.responses.empty()
